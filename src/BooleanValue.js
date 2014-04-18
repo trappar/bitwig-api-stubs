b41dbaaa2479246e671e52cf294a3fa982dfba18
Jeff Way <jway@repx.me>
2014-04-18 15:06:34 -0700

    Add finished BooleanValue

---
+++ src/BooleanValue.js
@@ -0,0 +1,13 @@
+function BooleanValue(){}
+
+/**
+ * @param {function} callback
+ */
+BooleanValue.prototype.addValueObserver = function(callback){};
+
+/**
+ * @param {boolean} value
+ */
+BooleanValue.prototype.set = function(value){};
+
+BooleanValue.prototype.toggle = function(){}; 
