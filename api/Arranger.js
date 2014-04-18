992a5b69645bbef6e50c3228f83de1c579107f6c
Jeff Way <jway@repx.me>
2014-04-18 13:33:54 -0700

    Finish Arranger

--- api/Arranger.js
+++ api/Arranger.js
@@ -1,3 +1,22 @@
 function Arranger(){
+    /**
+     * @param {function} callback
+     */
+    this.addCueMarkerVisibilityObserver = function(callback){};
 
+    /**
+     * @param {function} callback
+     */
+    this.addPlaybackFollowObserver = function(callback){};
+
+    /**
+     * @param {function} callback
+     */
+    this.addTrackRowHeightObserver = function(callback){};
+
+    this.toggleCueMarkerVisibility = function(){};
+
+    this.togglePlaybackFollow = function(){};
+
+    this.toggleTrackRowHeight = function(){};
 }
