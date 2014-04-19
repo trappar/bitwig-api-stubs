1152628ccb8f88234f2402cfb43dc85a70b424cd
Jeff Way <jway@repx.me>
2014-04-18 20:35:04 -0700

    Add global functions that I'm aware of (had to make this stuff up, it's not documented)

---
+++ src/Global.js
@@ -0,0 +1,11 @@
+/**
+ * Must be called for the API to work
+ * @param {number} version
+ */
+function loadAPI(version){}
+
+/**
+ * Load another script into this script (include)
+ * @param {string} script - path to script
+ */
+function load(script){}
