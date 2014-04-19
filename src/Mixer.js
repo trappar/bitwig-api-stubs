4703af2a39fb914a05c74c7752436b4de1b91a05
Jeff Way <jway@repx.me>
2014-04-18 17:10:21 -0700

    Add final mixer

---
+++ src/Mixer.js
@@ -0,0 +1,43 @@
+function Mixer(){}
+
+/**
+ * @param {function} callback
+ */
+Mixer.prototype.addClipLauncherSectionVisibilityObserver = function(callback){};
+
+/**
+ * @param {function} callback
+ */
+Mixer.prototype.addCrossFadeSectionVisibilityObserver = function(callback){};
+
+/**
+ * @param {function} callback
+ */
+Mixer.prototype.addDeviceSectionVisibilityObserver = function(callback){};
+
+/**
+ * @param {function} callback
+ */
+Mixer.prototype.addIoSectionVisibilityObserver = function(callback){};
+
+/**
+ * @param {function} callback
+ */
+Mixer.prototype.addMeterSectionVisibilityObserver = function(callback){};
+
+/**
+ * @param {function} callback
+ */
+Mixer.prototype.addSendsSectionVisibilityObserver = function(callback){};
+
+Mixer.prototype.toggleClipLauncherSectionVisibility = function(){};
+
+Mixer.prototype.toggleCrossFadeSectionVisibility = function(){};
+
+Mixer.prototype.toggleDeviceSectionVisibility = function(){};
+
+Mixer.prototype.toggleIoSectionVisibility = function(){};
+
+Mixer.prototype.toggleMeterSectionVisibility = function(){};
+
+Mixer.prototype.toggleSendsSectionVisibility = function(){};
