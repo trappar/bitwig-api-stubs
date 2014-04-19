c1f347a36a06bcbc2dabcf7ebe9f1268c7cda6d5
Jeff Way <jway@repx.me>
2014-04-18 17:41:29 -0700

    Add final ModulationSource

---
+++ src/ModulationSource.js
@@ -0,0 +1,15 @@
+function ModulationSource(){}
+
+/**
+ * @param {function} callback
+ */
+ModulationSource.prototype.addIsMappingObserver = function(callback){};
+
+/**
+ * @param {int} numChars
+ * @param {string}textWhenUnassigned
+ * @param {function} callback
+ */
+ModulationSource.prototype.addNameObserver = function(numChars, textWhenUnassigned, callback){};
+
+ModulationSource.prototype.toggleIsMapping = function(){}; 
